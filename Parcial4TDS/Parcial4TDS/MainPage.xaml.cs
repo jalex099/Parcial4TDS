473cbb0e25025a3827446fa38be3b7ba89c23d3a
jalex099 <mm18014@ues.edu.sv>
2020-12-06 15:20:58 -0600

    getData

--- Parcial4TDS/Parcial4TDS/MainPage.xaml.cs
+++ Parcial4TDS/Parcial4TDS/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,9 +13,28 @@ namespace Parcial4TDS
 {
     public partial class MainPage : ContentPage
     {
+        public string _URL { get; set; }
+
+        public HttpClient cli { get; set; }
+        private ObservableCollection<Models.Data> data;
+        private ObservableCollection<Models.Data> Data
+        {
+            get { return Data; }
+            set { data = value; }
+        }
         public MainPage()
         {
             InitializeComponent();
+            _URL = "http://juanmajano-002-site5.btempurl.com/api/Datos";
+            cli = new HttpClient();
+            Data = new ObservableCollection<Models.Data>();
+
+        }
+
+        public async void getData()
+        {
+            var getData = await cli.GetStringAsync(_URL);
+            var items = JsonConvert.DeserializeObject<List<Models.Data>>(getData);
         }
     }
 }
---
+++ Parcial4TDS/Parcial4TDS/Models/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial4TDS.Models
+{
+    public class Data
+    {
+        public int IdPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+
+}
